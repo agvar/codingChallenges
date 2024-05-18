a39ed14069e30fd1f6b33d05d8057bd9b575b7a3
agvar <ajeebgeorge@gmail.com>
2024-05-18 07:59:54 -0400

    inital commit- json parser

---
+++ jsonParser/parser.py
@@ -0,0 +1,50 @@
+#!/home/agvar/Projects/python/codingChallenges/venv/bin/python
+import sys
+import os
+
+tokens = {
+'{' :'start',
+'}': 'end' ,
+'"' : 'quote',
+',' : 'separator'
+}
+
+def tokenizer(file):
+    tokenized_input = []
+    current = ''
+    for char in file_buffer:
+        if char in tokens:
+            tokenized_input.append(char)
+            if current :
+                tokenized_input.append(current)
+                current =''
+        current += char
+    parser(tokenized_input)
+
+def parser(tokenized_input) :
+    if not tokenized_input:
+        return 1
+    for token in tokenized_input :
+        print(token)
+    return 0
+
+if __name__ == "__main__":
+    try:
+        file_path = sys.argv[1]
+        if not file_path or not os.path.exists(file_path) :
+            raise FileNotFoundError
+        
+        with open(file_path,"r") as f:
+            file_buffer = f.read()
+        return_flag = tokenizer(file_buffer)
+        if return_flag :
+            print(f"Invalid parser, return {return_flag}")
+        else:
+            print(f"Valid parser, return {return_flag}")
+
+    except FileNotFoundError as e:
+        print(f"{file_path} not found ")
+        sys.exit(1)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
